3cc2e5bdcab02e9865015825723c1d08c9a981d9
codcod <nikos@linux.com>
2023-02-28 15:26:45 +0100

    Add new query

--- scripts/sql/queries.sql
+++ scripts/sql/queries.sql
@@ -12,6 +12,16 @@ group by w.user_id
 having s.status_id in (100)
 ;
 
+-- user's current requests
+select u.user_id, u.name, u.surname, w.parking_day, s.name as STATUS, max(w.timestamp)
+from workflow w
+join users u on u.user_id = w.user_id
+join statuses s on s.status_id = w.status_id
+where 
+    u.user_id = 1
+group by w.parking_day
+having s.status_id in (100)
+;
 
 -- how many wins so far each user has
 select u.user_id, u.name, u.surname, count(*) as WINS
@@ -65,6 +75,6 @@ where spot_id in (
 select w.timestamp, w.parking_day, s.name as STATUS
 from workflow w
 join statuses s on w.status_id = s.status_id
-where w.user_id = 1
+where w.user_id = 3
 order by w.timestamp
 ;
